9df4b734bc102a5b3a341929d6e7849fdd92585c
0xKilty <iankilty1@gmail.com>
2024-06-10 21:31:01 -0600

    war

--- main.c
+++ main.c
@@ -1,27 +1,28 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
 
-int* sieve_of_eratosthenes(int limit, int *primes_count) {
+int* primes;
+int primes_count;
+int base;
+
+int *sieve_of_eratosthenes(int limit, int *primes_count) {
     bool *is_prime = malloc((limit + 1) * sizeof(bool));
     if (is_prime == NULL) {
         perror("Failed to allocate memory");
         exit(EXIT_FAILURE);
     }
 
-    for (int i = 0; i <= limit; i++) {
+    for (int i = 0; i <= limit; i++)
         is_prime[i] = true;
-    }
 
     is_prime[0] = is_prime[1] = false;
 
     for (int i = 2; i * i <= limit; i++) {
-        if (is_prime[i]) {
-            for (int j = i * i; j <= limit; j += i) {
-                is_prime[j] = false;
-            }
-        }
+        if (!is_prime[i])
+            continue;
+        for (int j = i * i; j <= limit; j += i)
+            is_prime[j] = false;
     }
 
     *primes_count = 0;
@@ -48,29 +49,81 @@ int* sieve_of_eratosthenes(int limit, int *primes_count) {
     return primes;
 }
 
-int get_base_prime(int num, int* primes) {
-    int res = 1;
+int power(int base, int exponent) {
+    int result = 1;
+    while (exponent > 0) {
+        if (exponent & 1)
+            result *= base;
+        base *= base;
+        exponent >>= 1;
+    }
+    return result;
+}
+
+
+int base10_to_base_n(int num, int base) {
+    int remainders[100];
+    int length = 0;
+
     div_t int_div;
-    int i = 0;
     while (num > 0) {
-        int_div = div(num, 10);
-        res *= pow(primes[i], int_div.rem);
+        int_div = div(num, base);
+        remainders[length++] = int_div.rem;
         num = int_div.quot;
-        i++;
+    }
+
+    int result = 0;
+    for (int i = length - 1; i >= 0; i--)
+        result += remainders[i] * power(10, i);
+
+    return result;
+}
+
+
+long long get_base_prime(int num, int base) {
+    long long res = 1;
+    div_t int_div;
+    for (int i = 0; num > 0; i++) {
+        int_div = div(num, base);
+        res *= power(primes[i], int_div.rem);
+        num = int_div.quot;
     }
     return res;
 }
 
-int main() {
-    int limit = 200;
-    int primes_count;
+void initalize_primes(int limit) {
+    primes = sieve_of_eratosthenes(limit, &primes_count);
+}
 
-    int *primes = sieve_of_eratosthenes(limit, &primes_count);
-
-    int base_prime = get_base_prime(831704, primes);
-    printf("%d\n", base_prime);
-
+void free_primes() {
     free(primes);
+    primes = NULL;
+}
+
+void initalize_base(int base_) {
+    base = base_;
+}
+
+void print_base_primes(int layers) {
+    if (layers == 0)
+        goto hell;
+    
+    for (int i = 0; i > base; i++) {
+        
+    }
+
+hell:
+}
+
+int main() {
+    int layers = 5;
+    int limit = 10000;
+    initalize_primes(limit);
+    initalize_base(10);
+    
+
+    
+    free_primes();
 
     return 0;
 }
