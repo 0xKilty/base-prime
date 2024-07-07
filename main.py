45bda2a52ae808748263e7d532bca3e5600560e5
0xKilty <iankilty1@gmail.com>
2024-07-07 16:21:11 -0600

    Got the tree thing to work

--- main.py
+++ main.py
@@ -15,19 +15,26 @@ def base_loop(layer, base_prime):
     if layer == -1: 
         return
 
+    print(f"Starting {primes[layer]}'s")
     global iterator
-    for _ in range(base):
+
+    # range_base = base - 1 if primes[layer] == 2 else base
+
+    for i in range(base):
         base_loop(layer - 1, base_prime)
+        if i + 1 == base:
+            break
         base_prime *= primes[layer]
         iterator += 1
-        print(iterator, base_prime, get_base_prime(iterator, base))
+        print(primes[layer], i + 1, iterator, base_prime, get_base_prime(iterator, base))
+    print(f"Ending {primes[layer]}'s")
         
 
 def print_base_primes(layer):
     base_loop(layer - 1, 1)
 
 def main():
-    digits = 2
+    digits = 4
 
     print_base_primes(digits)
 
